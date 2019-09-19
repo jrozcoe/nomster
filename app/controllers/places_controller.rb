ec68478d6f92b8d2cfef6d7ede917c714648dc9a
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2019-09-19 20:00:27 +0000

    Nomster lesson 25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user, only [:new, :create]
+  before_action :authenticate_user, only [:new, :create, :edit, :update]
 
   def index
     @places = Place.all
@@ -20,10 +20,15 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status :forbidden
     @place.update_attributes(place_params)
     redirect_to root_path
   end
