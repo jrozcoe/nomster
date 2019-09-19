61f0c70e1a4ca175d3b3696b4bfd2f38522ce3a2
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2019-09-19 20:11:14 +0000

    Nomster lesson 26

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user, only [:new, :create, :edit, :update]
+  before_action :authenticate_user, only [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all
@@ -35,8 +35,12 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+
     @place.destroy
     redirect_to root_path
+
 end
   end
 
