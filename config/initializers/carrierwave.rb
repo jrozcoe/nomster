43ad9d33bbd36e7d04f64f031ef3dc05a872046e
jrozcoe <54443883+jrozcoe@users.noreply.github.com>
2019-10-01 15:24:21 -0400

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIASHQPC44TCYFXXGVT"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["jroszkowiakbucket"]     
-
-           # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
