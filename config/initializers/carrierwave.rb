e849b8d90a67399e2ad743f42c835f81f0257f48
Jordan Roszkowiak <jordanroszkowiak@gmail.com>
2019-10-01 19:07:23 +0000

    edit API key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,10 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    aws_access_key_id:     ENV["AKIASHQPC44TCYFXXGVT"],        # required
+    aws_secret_access_key: ENV["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"],        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["jroszkowiakbucket"]     
+
+           # required
 end
